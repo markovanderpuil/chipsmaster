5b8a8599e3880e5c98ca40a23b2d70a5372b7931
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2015-12-04 16:18:41 +0100

    Expose available API

--- src/service.js
+++ src/service.js
@@ -2,7 +2,7 @@ var http = require("http");
 module.exports.createService = function(){
   return { start: function(port,callback){
     var server = http.createServer(function(request,response){
-      response.end();
+      response.end("{available_POST_endpoints: [move_to_pot]}");
     });
     server.listen(port,callback);
   }};
--- test/test_service.js
+++ test/test_service.js
@@ -3,12 +3,16 @@ var expect = require("expect.js"),
     
     CMS = require("../src/service");
 describe("The ChipsMaster Service", function() {
-  it("can have it's API queried",function(done){
+  it("can have its API queried",function(done){
     var service = CMS.createService();
     service.start(4242, function(){
-      request("http://localhost:4242",function(error,response,body) {
-        if (error) done(error);
-        else done();
+      request("http://localhost:4242/api",function(error,response,body) {
+        if (error) {
+          done(error);
+          return;
+        }
+        expect(body).to.equal("{available_POST_endpoints: [move_to_pot]}");
+        done();
       });
     });
   });
